7083c30691a65d43c7574d6a720f7f0dd6f82a04
Ishaangg <86571980+Ishaangg@users.noreply.github.com>
2024-01-31 12:47:19 +0530

    Create file

---
+++ 2530-MaximalScoreAfterApplyingKOperations/2530-MaximalScoreAfterApplyingKOperations.py
@@ -0,0 +1,20 @@
+from math import ceil
+import heapq
+class Solution:
+    def maxKelements(self, nums: List[int], k: int) -> int:
+        pq = [-num for num in nums]
+
+        for i in range(k):
+            element = -heapq.heappop(pq)
+
+        heapq.heapify(pq)
+            element = ceil(element/3)
+        score = []
+            score.append(element)
+
+        remaining_sum = sum(score)
+            heapq.heappush(pq, -element)
+
+        return remaining_sum
+
+[
