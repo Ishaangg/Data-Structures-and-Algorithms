6a5a4523f72f1e1409615dff5809de42608647b5
Ishaangg <86571980+Ishaangg@users.noreply.github.com>
2024-01-31 16:52:06 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximize Toys/maximize-toys.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+
+class Solution:
+    def toyCount(self, N, K, arr):
+        
+        # code here
+        arr.sort()
+        count = 0
+        i = 0
+
+        while i<N and K >= arr[i]:
+            K -= arr[i]
+            i += 1
+            count += 1
+            
+        return count
+            
+            
+                
+            
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N, K = [int(x) for x in input().split()]
+        arr = input().split()
+        for it in range(N):
+            arr[it] = int(arr[it])
+        
+        ob = Solution()
+        print(ob.toyCount(N, K, arr))
+# } Driver Code Ends
