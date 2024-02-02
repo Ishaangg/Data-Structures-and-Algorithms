27f3b3d41947f31bc200286d8fc5860d96c0ce93
Ishaangg <86571980+Ishaangg@users.noreply.github.com>
2024-02-02 21:44:21 +0530

    Update file 02/02/2024, 21:44:22

--- 1710-MaximumUnitsonaTruck/1710-MaximumUnitsonaTruck.py
+++ 1710-MaximumUnitsonaTruck/1710-MaximumUnitsonaTruck.py
@@ -1,13 +1,19 @@
-        boxTypes.sort(key = lambda x: x[1], reverse = True)
-    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-class Solution:
-        ans = 0
-        for box in boxTypes:
-            x = min(box[0], truckSize)
-            ans += (x*box[1])
-            truckSize -= x
-            if truckSize == 0:
-                break
-        return ans
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = [str(num) for num in nums]
 
+        nums.sort(key=LargerNumKey)
+class Solution:
+
+
+        largest_num = ''.join(nums)
+
+    def __lt__(x, y):
+class LargerNumKey(str):
+        return x+y·>·y+x 
+        return "0" if largest_num[0] == "0" else largest_num
+
+
+
+        
+                
 [
