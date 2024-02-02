547485331a3d9378df85ab9cc6159ecd98459349
Ishaangg <86571980+Ishaangg@users.noreply.github.com>
2024-02-02 21:48:40 +0530

    Create file

---
+++ 179-LargestNumber/179-LargestNumber.py
@@ -0,0 +1,19 @@
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = [str(num) for num in nums]
+
+        nums.sort(key=LargerNumKey)
+class Solution:
+
+
+        largest_num = ''.join(nums)
+
+    def __lt__(x, y):
+class LargerNumKey(str):
+        return x+y > y+x 
+        return largest_num
+
+
+
+        
+                
+[
