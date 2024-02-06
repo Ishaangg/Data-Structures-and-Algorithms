323a46036db0a9beef6ee2d519847da7d0a39921
Ishaangg <86571980+Ishaangg@users.noreply.github.com>
2024-02-06 16:16:22 +0530

    Create file

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,20 @@
+            if s[end] not in hset:
+                end += 1
+                count += 1
+                hset.add(s[end])
+                
+            else:
+
+        while end < len(s):
+        hset = set()
+
+                max_count = max(max_count, count)
+        max_count = 0
+                hset.remove(s[start])
+                count -= 1
+                start += 1
+                
+        return max_count
+
+
+"
