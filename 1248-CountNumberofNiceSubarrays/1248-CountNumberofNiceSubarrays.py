8a970aab66d14b67dcb5ee6f7700ad34936c5ab3
Ishaangg <86571980+Ishaangg@users.noreply.github.com>
2024-02-10 02:10:09 +0530

    Create file

---
+++ 1248-CountNumberofNiceSubarrays/1248-CountNumberofNiceSubarrays.py
@@ -0,0 +1,19 @@
+                while(nums[i]%2 == 0):
+
+                    i += 1
+                count = 0
+                temp += 1
+            if temp == k:
+                    count += 1
+                count += 1
+                i += 1
+            if nums[j]%2 != 0:
+
+                temp -= 1
+            
+            ans +=count
+            j+=1
+
+        return ans
+                                                 
+[
