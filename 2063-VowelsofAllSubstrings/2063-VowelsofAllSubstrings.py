ca4e2cb41ec14c6958ee88acdfa63fdc1f102f2c
Ishaangg <86571980+Ishaangg@users.noreply.github.com>
2024-02-11 23:09:20 +0530

    Create file

---
+++ 2063-VowelsofAllSubstrings/2063-VowelsofAllSubstrings.py
@@ -0,0 +1,16 @@
+        count_dict = {
+
+            "a",
+            "e",
+            "i",
+            "o",
+            "u"
+            }
+        for i in range(len(word)):
+            if word[i] in count_dict:
+                res += (n-i)*(i) + (n-i)
+
+        res = 0
+        return res
+
+"
